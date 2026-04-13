40c92d5070d91d5289379a336924ac9b095bc298
mmarhror <mohammedmarhror@gmail.com>
2026-04-13 12:14:20 +0100

    Quick Push

--- web/app/lib/data.ts
+++ web/app/lib/data.ts
@@ -2,5 +2,11 @@ import clientApi from "./clientApi";
 import { Tab } from "./types/groups";
 
 export async function fetchGroups(activeTab: Tab, query: string) {
-  // clientApi.get()
+  const params = new URLSearchParams({
+    tab: activeTab,
+    query: query,
+  });
+
+  const resp = await clientApi.get(`/groups?${params.toString()}`);
+  return resp.groups || [];
 }
