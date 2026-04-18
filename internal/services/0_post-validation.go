c199fff0ebc631ca1440406bb05f1e44aaa02463
aoutrgua <outrgua1949@gmail.com>
2026-04-18 23:51:06 +0100

    init service & validate post and comment

--- internal/services/0_post-validation.go
+++ internal/services/0_post-validation.go
@@ -1,9 +1,53 @@
 package services
 
-import "soc-net/internal/types"
+import (
+	"errors"
+	"strings"
 
-func ValidatePostInput(input types.GroupInput) error {
-	
+	"soc-net/internal/types"
+)
+
+var (
+	ErrInvalidTitle          = errors.New("title is required and must be under 100 characters")
+	ErrInvalidDescription    = errors.New("description is required and must be under 800 characters")
+	ErrInvalidPrivacy        = errors.New("privacy must be public, private, or almost private")
+	ErrInvalidCommentContent = errors.New("comment content is required and must be under 200 characters")
+	ErrInvalidImage          = errors.New("image url cannot be empty if provided")
+)
+
+func ValidatePostInput(input types.PostInput) error {
+	title := strings.TrimSpace(input.Title)
+	if title == "" || len(title) > 100 {
+		return ErrInvalidTitle
+	}
+
+	desc := strings.TrimSpace(input.Description)
+	if desc == "" || len(desc) > 800 {
+		return ErrInvalidDescription
+	}
+
+	if input.GroupId == nil {
+		if input.Privacy != "public" && input.Privacy != "private" && input.Privacy != "almost private" {
+			return ErrInvalidPrivacy
+		}
+	}
+
+	if input.ImageUrl != nil && strings.TrimSpace(*input.ImageUrl) == "" {
+		return ErrInvalidImage
+	}
+
+	return nil
+}
+
+func ValidateCommentInput(input types.CommentInput) error {
+	content := strings.TrimSpace(input.Content)
+	if content == "" || len(content) > 200 {
+		return ErrInvalidCommentContent
+	}
+
+	if input.ImageUrl != nil && strings.TrimSpace(*input.ImageUrl) == "" {
+		return ErrInvalidImage
+	}
 
 	return nil
 }
--- internal/services/0_service.go
+++ internal/services/0_service.go
@@ -3,15 +3,17 @@ package services
 import "soc-net/internal/repositories"
 
 type Services struct {
-	Auth   *AuthService
-	Groups *GroupsService
-	Posts  *PostsService
+	Auth     *AuthService
+	Groups   *GroupsService
+	Posts    *PostsService
+	Comments *CommentsService
 }
 
 func New(r *repositories.Repos) *Services {
 	return &Services{
-		Auth:   NewAuthService(r.Auth),
-		Groups: NewGroupsService(r.Auth, r.Groups),
-		Posts:  NewPostsService(r.Auth, r.Posts),
+		Auth:     NewAuthService(r.Auth),
+		Groups:   NewGroupsService(r.Auth, r.Groups),
+		Posts:    NewPostsService(r.Posts, r.Groups),
+		Comments: NewCommentsService(r.Comments, r.Posts),
 	}
 }
