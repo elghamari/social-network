d6c56f70835cf995dcc911713cc49ec4dd138dc9
aoutrgua <outrgua1949@gmail.com>
2026-04-19 19:19:59 +0100

    update repositories

--- internal/repositories/0_repository.go
+++ internal/repositories/0_repository.go
@@ -3,17 +3,19 @@ package repositories
 import "database/sql"
 
 type Repos struct {
-	Auth   *AuthRepo
-	Groups *GroupsRepo
-	Posts  *PostsRepo
-	Comments *CommentsRepo
+	Auth      *AuthRepo
+	Groups    *GroupsRepo
+	Posts     *PostsRepo
+	Comments  *CommentsRepo
+	Reactions *ReactionsRepo
 }
 
 func New(db *sql.DB) *Repos {
 	return &Repos{
-		Auth:   NewAuthRepo(db),
-		Groups: NewGroupsRepo(db),
-		Posts:  NewPostsRepo(db),
-		Comments: NewCommentsRepo(db),
+		Auth:      NewAuthRepo(db),
+		Groups:    NewGroupsRepo(db),
+		Posts:     NewPostsRepo(db),
+		Comments:  NewCommentsRepo(db),
+		Reactions: NewReactionsRepo(db),
 	}
 }
