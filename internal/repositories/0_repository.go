4acd2eac3800866a425c271149f0b66d70a0cfb5
elghamari <mohammed.elghamari@gmail.com>
2026-04-22 23:24:59 +0100

    Add ChatRepo initialization in Repos constructor

--- internal/repositories/0_repository.go
+++ internal/repositories/0_repository.go
@@ -5,11 +5,13 @@ import "database/sql"
 type Repos struct {
 	Auth   *AuthRepo
 	Groups *GroupsRepo
+	Chat   *ChatRepo
 }
 
 func New(db *sql.DB) *Repos {
 	return &Repos{
 		Auth:   NewAuthRepo(db),
 		Groups: NewGroupsRepo(db),
+		Chat:   NewChatRepo(db),
 	}
 }
