7395c186d3e02c5fe78468da7e107cea83d485c1
melbouab <melbouabdelaoui@gmail.com>
2026-04-22 15:25:48 +0100

    updat sessions

--- web/proxy.ts
+++ web/proxy.ts
@@ -8,21 +8,18 @@ export async function proxy(request: NextRequest) {
 
   const isProtectedRoute = path === '/' || path.startsWith('/profile');
   const isAuthRoute = path === '/login' || path === '/register';
-if (isProtectedRoute) {
+
+  if (isProtectedRoute) {
     if (!sessionId) {
       return NextResponse.redirect(new URL('/login', request.url));
     }
 
     try {
-      // بدلنا localhost بـ 127.0.0.1 وتأكد من /api إيلا كانت عندك فـ Go
       const res = await fetch('http://localhost:8080/auth/check', {
         headers: {
           Cookie: `sessionId=${sessionId}`,
         },
       });
-
-      // هادي غتطبع لينا فـ terminal ديال VS Code شنو رجع Go
-      console.log("Go Server Status:", res.status); 
 
       if (!res.ok) {
         const response = NextResponse.redirect(new URL('/login', request.url));
@@ -30,11 +27,10 @@ if (isProtectedRoute) {
         return response;
       }
     } catch (err) {
-      // هادي غتطبع لينا إيلا كان السيرفور طافي ولا الرابط غالط
-      console.log("Fetch Error in Proxy:", err); 
       return NextResponse.redirect(new URL('/login', request.url));
     }
   }
+
   if (isAuthRoute && sessionId) {
     return NextResponse.redirect(new URL('/', request.url));
   }
