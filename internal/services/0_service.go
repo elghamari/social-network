8f17d70e0f619a9d4ce16e4d161a64bb47423edc
elghamari <mohammed.elghamari@gmail.com>
2026-04-22 23:31:41 +0100

    Initialize ChatService in Services constructor

--- internal/services/0_service.go
+++ internal/services/0_service.go
@@ -5,11 +5,13 @@ import "soc-net/internal/repositories"
 type Services struct {
 	Auth   *AuthService
 	Groups *GroupsService
+	Chat   *ChatService
 }
 
 func New(r *repositories.Repos) *Services {
 	return &Services{
 		Auth:   NewAuthService(r.Auth),
 		Groups: NewGroupsService(r.Auth, r.Groups),
+		Chat:   NewChatService(r.Chat, r.Groups),
 	}
 }
