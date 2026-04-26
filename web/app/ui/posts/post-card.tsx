a83901d221ae43c799f6fa46f1b6848f86a56fc0
aoutrgua <outrgua1949@gmail.com>
2026-04-26 01:11:53 +0100

    add toggle like post

--- web/app/ui/posts/post-card.tsx
+++ web/app/ui/posts/post-card.tsx
@@ -1,20 +1,50 @@
 "use client";
+import { FormatTime } from "@/app/lib/utils/format-time";
 import "./posts.css";
 import { PostType } from "@/app/lib/types/feed";
+import { useState } from "react";
+import { ToggleLikePost } from "@/app/lib/services/feed";
+import { showToast } from "../layout/toast-store";
 
 export default function PostCard({ post }: { post: PostType }) {
+
+  const [isLiked, setIsLiked] = useState(post.is_liked);
+  const [likesCount, setLikesCount] = useState(post.total_likes);
+  
+  const [isLiking, setIsLiking] = useState(false);
+
+  const handleLike = async () => {
+    if (isLiking) return;
+    
+    setIsLiking(true);
+    try {
+      const response = await ToggleLikePost(post.id);
+      
+      if (response && response.status === 200) {
+        setIsLiked(!isLiked);
+        setLikesCount(prev => isLiked ? prev - 1 : prev + 1);
+      }
+    } catch (error: any) {
+        showToast(`${error.message}`)
+        console.log("Error :", error);
+    } finally {
+      setIsLiking(false);
+    }
+  };
+
+
   return (
     <article className="post-card">
       <div className="post-card-header">
-        <div className="post-card-avatar">{post.author.initials}</div>
+        <div className="post-card-avatar">{post.author.avatar ? post.author.avatar : post.author.fistname?.[0]?.toUpperCase() || "?"}</div>
         
         <div className="post-card-meta">
           <div className="post-author-info">
-            <span className="post-author-name">{post.author.name}</span>
-            <span className="post-author-username">{post.author.username}</span>
+            <span className="post-author-name">{`${post.author.fistname} ${post.author.lastname}`}</span>
+            <span className="post-author-username">{`@${post.author.nickname}`}</span>
           </div>
           <div className="post-time-privacy">
-            <span>{post.timeAgo}</span>
+            <span>{FormatTime(post.created_at)}</span>
             <span className="dot-separator">•</span>
             <span>
               {post.privacy === "public" && "Public"}
@@ -30,26 +60,31 @@ export default function PostCard({ post }: { post: PostType }) {
         <h3 className="post-title">{post.title}</h3>
         <p className="post-description">{post.description}</p>
         
-        {post.imageUrl && (
+        {post.image_url && (
           <div className="post-image-container">
-            <img src={post.imageUrl} alt="Post content" className="post-image" />
+            <img src={post.image_url} alt="Post content" className="post-image" />
           </div>
         )}
       </div>
 
       <div className="post-card-footer">
-        <button className="post-action-btn">
-          <svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
-          </svg>
-          <span>{post.likesCount} Likes</span>
+        <button 
+          className={`post-action-btn ${isLiked ? 'like-active' : ''}`}
+          onClick={handleLike}
+          disabled={isLiking}
+        >
+          <svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor"
+            fill={isLiked ? "currentColor" : "none"}
+            strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>          </svg>
+          <span>{likesCount} Likes</span>
         </button>
 
         <button className="post-action-btn">
           <svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
           </svg>
-          <span>{post.commentsCount} Comments</span>
+          <span>{post.total_comments} Comments</span>
         </button>
       </div>
     </article>
