101609fc67b8f3073d9c35d1414a7c59efcea683
elghamari <mohammed.elghamari@gmail.com>
2026-04-26 01:03:22 +0100

    Enhance ClientApi with error handling and content type management; add feed service functions for post retrieval and creation

--- web/app/lib/services/client.ts
+++ web/app/lib/services/client.ts
@@ -1,22 +1,44 @@
 const API_BASE = "http://localhost:8080/api";
 
 class ClientApi {
-  async request(endPoint: string, options = {}) {
-    const config = {
+  async request(endPoint: string, options: RequestInit = {}) {
+
+    const isFormData = options.body instanceof FormData;
+
+    const headers: any = {
+      ...options.headers,
+    };
+
+    if (!isFormData) {
+      headers["Content-Type"] = "application/json";
+    }
+
+    const config: RequestInit = {
       ...options,
-      headers: { "Content-type": "application/json" },
+      headers,
+      credentials: "include",
     };
 
     try {
-      //
       const resp = await fetch(`${API_BASE}${endPoint}`, config);
+
+      if (!resp.ok) {
+        const errorData = await resp.json().catch(() => ({}));
+        throw {
+          status: resp.status,
+          message: errorData.error || "Request Failed",
+        };
+      }
+
       return resp.json();
 
-      //
-    } catch (err) {
+    } catch (err: any) {
+      console.log("ClientApi Error:", err);
+      
+      if (err.status) throw err;
       throw {
         status: 0,
-        message: "Request Failed",
+        message: "Network Error or Server Down",
         error: err,
       };
     }
@@ -26,14 +48,21 @@ class ClientApi {
     return this.request(endPoint);
   }
 
-  post(endPoint: string, data: {}) {
+  post(endPoint: string, data: any) {
     return this.request(endPoint, {
       method: "POST",
       body: JSON.stringify(data),
     });
   }
 
-  put(endPoint: string, data: {}) {
+  postForm(endPoint: string, formData: FormData) {
+    return this.request(endPoint, {
+      method: "POST",
+      body: formData,
+    });
+  }
+
+  put(endPoint: string, data: any) {
     return this.request(endPoint, {
       method: "PUT",
       body: JSON.stringify(data),
