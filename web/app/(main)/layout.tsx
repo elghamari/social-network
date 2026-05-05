e4de759b950e209de67213632b9f005cc52d37b2
elghamari <mohammed.elghamari@gmail.com>
2026-05-05 09:53:18 +0100

    Wrap MainLayout component with WebSocketProvider and add pathname check for chat page

--- web/app/(main)/layout.tsx
+++ web/app/(main)/layout.tsx
@@ -1,20 +1,29 @@
+"use client";
 import "../layout.css";
 import Sidenav from "@/app/ui/layout/sidenav";
 import RightSidebar from "@/app/ui/layout/right-sidebar";
 import { AuthProvider } from "@/app/_context/AuthContext";
+import { WebSocketProvider } from "../_context/WebSocketContext";
+import { usePathname } from "next/navigation";
+
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isChatPage = pathname.startsWith("/chat"); 
+
   return (
     <AuthProvider>
-      <div className="app">
-        <Sidenav />
-        <main className="app__content">{children}</main>
-        <RightSidebar />
-      </div>
+      <WebSocketProvider>
+        <div className="app">
+          <Sidenav />
+          <main className="app__content">{children}</main>
+          {!isChatPage && <RightSidebar />}
+        </div>
+      </WebSocketProvider>
     </AuthProvider>
   );
 }
