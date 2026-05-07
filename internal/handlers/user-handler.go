b1f66e4aa86f633fd6950398bca0af0334e05d32
aoutrgua <outrgua1949@gmail.com>
2026-05-07 11:52:29 +0100

    fix bug

--- internal/handlers/user-handler.go
+++ internal/handlers/user-handler.go
@@ -127,44 +127,43 @@ func (h *Handler) UnfollowUser(w http.ResponseWriter, r *http.Request) {
 
 // GET /api/me
 func (h *Handler) GetMe(w http.ResponseWriter, r *http.Request) {
-    userID := utils.GetUserId(r)
+	userID := utils.GetUserId(r)
 
-    user, err := h.Services.Auth.GetUserById(userID)
-    if err != nil {
-        utils.WriteJson(w, http.StatusNotFound, map[string]any{"error": "user not found"})
-        return
-    }
+	user, err := h.Services.Auth.GetUserById(userID)
+	if err != nil {
+		utils.WriteJson(w, http.StatusNotFound, map[string]any{"error": "user not found"})
+		return
+	}
 
-    safeStr := func(s *string) string {
-        if s == nil {
-            return ""
-        }
-        return *s
-    }
+	safeStr := func(s *string) string {
+		if s == nil {
+			return ""
+		}
+		return *s
+	}
 
-    followers, _ := h.Services.User.GetFollowers(userID)
-    following, _ := h.Services.User.GetFollowing(userID)
-    pending, _ := h.Services.User.GetPendingRequests(userID)
+	followers, _ := h.Services.User.GetFollowers(userID)
+	following, _ := h.Services.User.GetFollowing(userID)
+	pending, _ := h.Services.User.GetPendingRequests(userID)
 
-    utils.WriteJson(w, http.StatusOK, map[string]any{
-        "user": map[string]any{ 
-            "id":               user.ID,
-            "first_name":       user.FirstName,
-            "last_name":        user.LastName,
-            "is_public":        user.IsPublic,
-            "email":            user.Email,
-            "avatar":           safeStr(user.Avatar),  
-            "about_me":         safeStr(user.AboutMe),  
-            "nickname":         safeStr(user.Nickname), 
-            "date_of_birth":    user.DateOfBirth,
-            "follow_status":    "owner",
-            "followers":        followers,
-            "following":        following,
-            "pending_requests": pending,
-        },
-    })
+	utils.WriteJson(w, http.StatusOK, map[string]any{
+		"user": map[string]any{
+			"id":               user.ID,
+			"first_name":       user.FirstName,
+			"last_name":        user.LastName,
+			"is_public":        user.IsPublic,
+			"email":            user.Email,
+			"avatar":           safeStr(user.Avatar),
+			"about_me":         safeStr(user.AboutMe),
+			"nickname":         safeStr(user.Nickname),
+			"date_of_birth":    user.DateOfBirth,
+			"follow_status":    "owner",
+			"followers":        followers,
+			"following":        following,
+			"pending_requests": pending,
+		},
+	})
 }
-
 
 // GET /api/profile?profile_id=<id>
 func (h *Handler) GetProfile(w http.ResponseWriter, r *http.Request) {
@@ -198,6 +197,12 @@ func (h *Handler) GetProfile(w http.ResponseWriter, r *http.Request) {
 			pending, _ = h.Services.User.GetPendingRequests(targetID)
 		}
 	}
+	safeStr := func(s *string) string {
+		if s == nil {
+			return ""
+		}
+		return *s
+	}
 
 	utils.WriteJson(w, http.StatusOK, map[string]any{
 		"user": types.UserProfileResponse{
@@ -209,9 +214,9 @@ func (h *Handler) GetProfile(w http.ResponseWriter, r *http.Request) {
 			Followers:       followers,
 			Following:       following,
 			Email:           target.Email,
-			Avatar:          *target.Avatar,
-			AboutMe:         *target.AboutMe,
-			Nickname:        *target.Nickname,
+			Avatar:          safeStr(target.Avatar),
+			AboutMe:         safeStr(target.AboutMe),
+			Nickname:        safeStr(target.Nickname),
 			DateOfBirth:     target.DateOfBirth,
 			PendingRequests: pending,
 		},
